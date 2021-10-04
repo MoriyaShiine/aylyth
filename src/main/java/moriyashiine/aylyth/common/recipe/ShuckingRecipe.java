c2fb9bc8677182e68dbaa25cdd872947441420d0
Aelpecyem <aelpecyem@gmail.com>
2021-10-04 18:02:48 +0200

    fix shucking recipe

--- src/main/java/moriyashiine/aylyth/common/recipe/ShuckingRecipe.java
+++ src/main/java/moriyashiine/aylyth/common/recipe/ShuckingRecipe.java
@@ -3,10 +3,12 @@ package moriyashiine.aylyth.common.recipe;
 import moriyashiine.aylyth.common.registry.ModItems;
 import moriyashiine.aylyth.common.registry.ModRecipeTypes;
 import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialCraftingRecipe;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -42,23 +44,25 @@ public class ShuckingRecipe extends SpecialCraftingRecipe {
 	
 	@Override
 	public ItemStack craft(CraftingInventory inv) {
-		//todo make this work
-		return ItemStack.EMPTY;
-		//		ItemStack dagger = null, fruit = null;
-		//		for (int i = 0; i < inv.size(); i++) {
-		//			ItemStack stack = inv.getStack(i);
-		//			if (stack.isOf(ModItems.YMPE_DAGGER)) {
-		//				dagger = stack.copy();
-		//				dagger.damage(1, RANDOM, null);
-		//			}
-		//			else if (stack.isOf(ModItems.YMPE_FRUIT)) {
-		//				fruit = stack.copy();
-		//				fruit.decrement(1);
-		//			}
-		//		}
-		//		return new ItemStack(ModItems.SHUCKED_YMPE_FRUIT);
+		return new ItemStack(ModItems.SHUCKED_YMPE_FRUIT);
 	}
-	
+
+	@Override
+	public DefaultedList<ItemStack> getRemainder(CraftingInventory inventory) {
+		DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(inventory.size(), ItemStack.EMPTY);
+
+		for(int i = 0; i < defaultedList.size(); ++i) {
+			ItemStack stack = inventory.getStack(i);
+			if (stack.isOf(ModItems.YMPE_DAGGER)) {
+				ItemStack dagger = stack.copy();
+				dagger.damage(1, RANDOM, null);
+				defaultedList.set(i, dagger);
+			}
+		}
+
+		return defaultedList;
+	}
+
 	@Override
 	public boolean fits(int width, int height) {
 		return true;
