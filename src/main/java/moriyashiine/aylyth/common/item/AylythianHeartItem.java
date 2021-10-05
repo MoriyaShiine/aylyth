137d8826dc01fb0d9b49e7e25fcdafbcfbfbe767
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-10-05 10:22:57 -0500

    aylythian heart right click functionality minus safe teleport

--- src/main/java/moriyashiine/aylyth/common/item/AylythianHeartItem.java
+++ src/main/java/moriyashiine/aylyth/common/item/AylythianHeartItem.java
@@ -1,6 +1,10 @@
 package moriyashiine.aylyth.common.item;
 
+import moriyashiine.aylyth.common.registry.ModDimensions;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,6 +12,9 @@ import net.minecraft.item.ItemUsage;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
 
 public class AylythianHeartItem extends Item {
@@ -17,13 +24,28 @@ public class AylythianHeartItem extends Item {
 	
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		return ItemUsage.consumeHeldItem(world, user, hand);
+		if (user.isSneaking()) {
+			if (user.isCreative() || user.experienceLevel >= 5) {
+				if (user.world.getRegistryKey() != ModDimensions.AYLYTH) {
+					return ItemUsage.consumeHeldItem(world, user, hand);
+				}
+			}
+		}
+		return super.use(world, user, hand);
 	}
 	
 	@Override
 	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-		if (!world.isClient && user instanceof PlayerEntity player && player.experienceLevel >= 5) {
-			player.addExperience(-55);
+		if (!world.isClient && user instanceof PlayerEntity player && world.getRegistryKey() != ModDimensions.AYLYTH) {
+			if (player.isCreative() || player.experienceLevel >= 5) {
+				FabricDimensions.teleport(user, world.getServer().getWorld(ModDimensions.AYLYTH), new TeleportTarget(Vec3d.of(getSafePosition(world, user.getBlockPos())), Vec3d.ZERO, user.headYaw, user.getPitch()));
+				user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200));
+				user.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200));
+				if (!player.isCreative()) {
+					player.addExperience(-55);
+					stack.decrement(1);
+				}
+			}
 		}
 		return super.finishUsing(stack, world, user);
 	}
@@ -36,5 +58,10 @@ public class AylythianHeartItem extends Item {
 	@Override
 	public int getMaxUseTime(ItemStack stack) {
 		return 40;
+	}
+	
+	//todo put the player in a safe position
+	public static BlockPos getSafePosition(World world, BlockPos pos) {
+		return pos;
 	}
 }
