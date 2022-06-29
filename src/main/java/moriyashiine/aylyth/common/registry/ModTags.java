621007fa5c78f1dd19e7fc5c1a02d005aadc175e
dhyces <pokmon987@gmail.com>
2022-06-29 11:31:32 -0400

    Add "generates_seep" biome tags

--- src/main/java/moriyashiine/aylyth/common/registry/ModTags.java
+++ src/main/java/moriyashiine/aylyth/common/registry/ModTags.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 
 public class ModTags {
 	public static final TagKey<EntityType<?>> GRIPWEED_IMMUNE = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(Aylyth.MOD_ID, "gripweed_immune"));
@@ -14,4 +15,8 @@ public class ModTags {
 	public static final TagKey<Block> SEEPS = TagKey.of(Registry.BLOCK_KEY, new Identifier(Aylyth.MOD_ID, "seeps"));
 
 	public static final TagKey<Item> YMPE_FOODS = TagKey.of(Registry.ITEM_KEY, new Identifier(Aylyth.MOD_ID, "ympe_foods"));
+
+	public static final TagKey<Biome> GENERATES_OAK_SEEP = TagKey.of(Registry.BIOME_KEY, new Identifier(Aylyth.MOD_ID, "generates_oak_seep"));
+	public static final TagKey<Biome> GENERATES_SPRUCE_SEEP = TagKey.of(Registry.BIOME_KEY, new Identifier(Aylyth.MOD_ID, "generates_spruce_seep"));
+	public static final TagKey<Biome> GENERATES_DARK_OAK_SEEP = TagKey.of(Registry.BIOME_KEY, new Identifier(Aylyth.MOD_ID, "generates_dark_oak_seep"));
 }
