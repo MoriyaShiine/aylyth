f98cfd08084ceac4345e5bb250b6cd0333ef9aff
dhyces <pokmon987@gmail.com>
2022-07-17 19:17:17 -0400

    Createdd AylythNoiseSettings
    This lets us datagen the noise settings (when they're figured out)

--- src/main/java/moriyashiine/aylyth/datagen/AylythDatagen.java
+++ src/main/java/moriyashiine/aylyth/datagen/AylythDatagen.java
@@ -2,15 +2,13 @@ package moriyashiine.aylyth.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.data.report.WorldgenProvider;
-import net.minecraft.data.server.BiomeParametersProvider;
 
 public class AylythDatagen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         boolean runClient = true;
         boolean runServer = true;
+        AylythNoiseSettings.init();
         fabricDataGenerator.addProvider(runClient, ModModelProvider::new);
         fabricDataGenerator.addProvider(runServer, ModBiomeTagProvider::new);
         fabricDataGenerator.addProvider(runServer, ModWorldgenProvider::new);
