4af2cb4edf2d7c31ce18813a2538438a92c09e1c
dhyces <pokmon987@gmail.com>
2022-09-06 17:41:07 -0400

    update with some more noise and functions

--- src/main/java/moriyashiine/aylyth/datagen/AylythNoiseTypes.java
+++ src/main/java/moriyashiine/aylyth/datagen/AylythNoiseTypes.java
@@ -23,9 +23,24 @@ public class AylythNoiseTypes {
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> BASE_LAYER_KEY = noiseKey("base_layer");
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> CAVE_CHEESE_KEY = noiseKey("cave_cheese");
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> CAVE_ENTRANCES_KEY = noiseKey("cave_entrances");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_KEY = noiseKey("spaghetti_2d");
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_MODULATOR_KEY = noiseKey("spaghetti_2d_modulator");
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_ROUGHNESS_KEY = noiseKey("spaghetti_roughness");
     public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_THICKNESS_KEY = noiseKey("spaghetti_2d_thickness");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_ELEVATION_KEY = noiseKey("spaghetti_2d_elevation");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_1_KEY = noiseKey("spaghetti_3d_1");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_2_KEY = noiseKey("spaghetti_3d_2");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_RARITY_KEY = noiseKey("spaghetti_3d_rarity");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_THICKNESS_KEY = noiseKey("spaghetti_3d_thickness");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_KEY = noiseKey("noodle");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_THICKNESS_KEY = noiseKey("noodle_thickness");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_RIDGE_A_KEY = noiseKey("noodle_ridge_a");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_RIDGE_B_KEY = noiseKey("noodle_ridge_b");
+
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> PILLAR_KEY = noiseKey("pillar");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> PILLAR_RARENESS_KEY = noiseKey("pillar_rareness");
+    public static final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> PILLAR_THICKNESS_KEY = noiseKey("pillar_thickness");
+
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> OFFSET;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> RIDGE;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> JAGGED;
@@ -36,9 +51,23 @@ public class AylythNoiseTypes {
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> BASE_LAYER;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> CAVE_CHEESE;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> CAVE_ENTRANCES;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_MODULATOR;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_ROUGHNESS;
     public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_THICKNESS;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_2D_ELEVATION;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_1;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_2;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_RARITY;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> SPAGHETTI_3D_THICKNESS;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> NOODLE;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_THICKNESS;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_RIDGE_A;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> NOODLE_RIDGE_B;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> PILLAR;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> PILLAR_RARENESS;
+    public static final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> PILLAR_THICKNESS;
+
 
     private static RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> noiseKey(String id) {
         return RegistryKey.of(Registry.NOISE_KEY, new Identifier(Aylyth.MOD_ID, id));
@@ -59,8 +88,45 @@ public class AylythNoiseTypes {
         BASE_LAYER = registerNoise(BASE_LAYER_KEY,-8, 1.0, 1.0, 1.0, 1.0);
         CAVE_CHEESE = registerNoise(CAVE_CHEESE_KEY, -8, 0.5, 1.0, 2.0, 1.0, 2.0, 1.0, 0.0, 2.0, 0.0);
         CAVE_ENTRANCES = registerNoise(CAVE_ENTRANCES_KEY, -7, 0.4, 0.5, 1.0);
+        SPAGHETTI_2D = registerNoise(SPAGHETTI_2D_KEY, -7, 1.0);
         SPAGHETTI_2D_MODULATOR = registerNoise(SPAGHETTI_2D_MODULATOR_KEY, -11, 1.0);
         SPAGHETTI_ROUGHNESS = registerNoise(SPAGHETTI_ROUGHNESS_KEY, -5, 1.0);
         SPAGHETTI_2D_THICKNESS = registerNoise(SPAGHETTI_2D_THICKNESS_KEY, -11, 1.0);
+        SPAGHETTI_2D_ELEVATION = registerNoise(SPAGHETTI_2D_ELEVATION_KEY, -8, 1.0);
+        SPAGHETTI_3D_1 = registerNoise(SPAGHETTI_3D_1_KEY, -7, 1.0);
+        SPAGHETTI_3D_2 = registerNoise(SPAGHETTI_3D_2_KEY, -7, 1.0);
+        SPAGHETTI_3D_RARITY = registerNoise(SPAGHETTI_3D_RARITY_KEY, -11, 1.0);
+        SPAGHETTI_3D_THICKNESS = registerNoise(SPAGHETTI_3D_THICKNESS_KEY, -11, 1.0);
+        NOODLE = registerNoise(NOODLE_KEY, -8, 1.0);
+        NOODLE_THICKNESS = registerNoise(NOODLE_THICKNESS_KEY, -8, 1.0);
+        NOODLE_RIDGE_A = registerNoise(NOODLE_RIDGE_A_KEY, -7, 1.0);
+        NOODLE_RIDGE_B = registerNoise(NOODLE_RIDGE_B_KEY, -7, 1.0);
+        PILLAR = registerNoise(PILLAR_KEY, -7, 1.0, 1.0);
+        PILLAR_RARENESS = registerNoise(PILLAR_RARENESS_KEY, -8, 1.0);
+        PILLAR_THICKNESS = registerNoise(PILLAR_THICKNESS_KEY, -8, 1.0);
     }
+
+//    static class NoiseRegistryPair {
+//        final RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> registryKey;
+//        final RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> registryEntry;
+//
+//        private NoiseRegistryPair(RegistryKey<DoublePerlinNoiseSampler.NoiseParameters> registryKey, RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> registryEntry) {
+//            this.registryKey = registryKey;
+//            this.registryEntry = registryEntry;
+//        }
+//
+//        static NoiseRegistryPair register(String id, int firstOctave, double... amplitudes) {
+//            var registryKey = RegistryKey.of(Registry.NOISE_KEY, new Identifier(Aylyth.MOD_ID, id));
+//            var noiseParams = new DoublePerlinNoiseSampler.NoiseParameters(firstOctave, DoubleList.of(amplitudes));
+//            var registryEntry = BuiltinRegistries.add(BuiltinRegistries.NOISE_PARAMETERS, registryKey, noiseParams);
+//            return new NoiseRegistryPair(registryKey, registryEntry);
+//        }
+//
+//        public static NoiseRegistryPair register(Identifier identifier, int firstOctave, double... amplitudes) {
+//            var registryKey = RegistryKey.of(Registry.NOISE_KEY, identifier);
+//            var noiseParams = new DoublePerlinNoiseSampler.NoiseParameters(firstOctave, DoubleList.of(amplitudes));
+//            var registryEntry = BuiltinRegistries.add(BuiltinRegistries.NOISE_PARAMETERS, registryKey, noiseParams);
+//            return new NoiseRegistryPair(registryKey, registryEntry);
+//        }
+//    }
 }
