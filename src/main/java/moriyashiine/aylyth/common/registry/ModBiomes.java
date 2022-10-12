71df63bccde0b07effe31668584e2f6ddfb33fa6
dhyces <pokmon987@gmail.com>
2022-10-11 20:50:45 -0400

    Biome reg-help
    - add another factory method with precipitation, temp, and downfall built in
    - add two (double) convenience methods for temp and downfall
    - add a method for biome effects with the necessary stuff forced and a consumer to add more
    
    Creation of Biome reg-help
    
    - also changed color int values to hex
    
    Update BiomeEffectsBuilder.java
    
    - add convenience method for particle configs
    
    Fix GenerationSettingsBuilder
    
    - the "feature" consumer method was also used for carvers, so needed to add that capability to it. also renamed it to "add"
    
    Put new biome builder features to use
    
    Update ModBiomes.java

--- src/main/java/moriyashiine/aylyth/common/registry/ModBiomes.java
+++ src/main/java/moriyashiine/aylyth/common/registry/ModBiomes.java
@@ -1,6 +1,8 @@
 package moriyashiine.aylyth.common.registry;
 
 import moriyashiine.aylyth.common.Aylyth;
+import moriyashiine.aylyth.common.registry.biome.BiomeBuilder;
+import moriyashiine.aylyth.common.registry.biome.SpawnSettingsBuilder;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.BiomeAdditionsSound;
@@ -19,8 +21,8 @@ import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
 //TODO: springs and clearing flowers still need fixed
 public class ModBiomes {
-	public static final SpawnSettings.Builder COPSE_MOBS = new SpawnSettings.Builder().spawnCost(ModEntityTypes.AYLYTHIAN, 2, 1).spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ModEntityTypes.AYLYTHIAN, 20, 1, 2)).spawn(SpawnGroup.AMBIENT, new SpawnSettings.SpawnEntry(ModEntityTypes.PILOT_LIGHT, 5, 1, 1)).creatureSpawnProbability(0.5F);
-	public static final SpawnSettings.Builder DEEPWOOD_MOBS = new SpawnSettings.Builder().spawnCost(ModEntityTypes.AYLYTHIAN, 3, 1).spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ModEntityTypes.AYLYTHIAN, 25, 1, 3)).spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ModEntityTypes.ELDER_AYLYTHIAN, 2, 1, 1)).spawn(SpawnGroup.AMBIENT, new SpawnSettings.SpawnEntry(ModEntityTypes.PILOT_LIGHT, 10, 1, 1));
+	public static final SpawnSettings COPSE_MOBS = SpawnSettingsBuilder.builder().spawnCost(ModEntityTypes.AYLYTHIAN, 2, 1).monster(ModEntityTypes.AYLYTHIAN, 20, 1, 2).ambient(ModEntityTypes.PILOT_LIGHT, 5, 1, 1).spawnChance(0.5F).build();
+	public static final SpawnSettings DEEPWOOD_MOBS = SpawnSettingsBuilder.builder().spawnCost(ModEntityTypes.AYLYTHIAN, 3, 1).monster(ModEntityTypes.AYLYTHIAN, 25, 1, 3).monster(ModEntityTypes.ELDER_AYLYTHIAN, 2, 1, 1).ambient(ModEntityTypes.PILOT_LIGHT, 10, 1, 1).build();
 	public static final BiomeAdditionsSound OVERGROWN_CLEARING_AMBIANCE = new BiomeAdditionsSound(ModSoundEvents.AMBIENT_FOREST_ADDITIONS, 0.001);
 	public static final BiomeAdditionsSound FOREST_AMBIANCE = new BiomeAdditionsSound(ModSoundEvents.AMBIENT_FOREST_ADDITIONS, 0.005);
 	public static final RegistryKey<Biome> CLEARING_ID = RegistryKey.of(Registry.BIOME_KEY, new Identifier(Aylyth.MOD_ID, "clearing"));
@@ -32,96 +34,128 @@ public class ModBiomes {
 	public static final RegistryKey<Biome> UPLANDS_ID = RegistryKey.of(Registry.BIOME_KEY, new Identifier(Aylyth.MOD_ID, "uplands"));
 	private static final int AYLYTHIAN_FOLIAGE_COLOR = 0x627F38;
 	private static final int DEEP_AYLYTHIAN_FOLIAGE_COLOR = 0x9E811A;
-	private static final int WATER_COLOR = 4159204;
-	private static final int UNDERWATER_COLOR = 329011;
+	private static final int WATER_COLOR = 0x3F76E4;
+	private static final int UNDERWATER_COLOR = 0x050533;
 	private static final int FOG_COLOR = 0x666666;
 	private static final int SKY_COLOR = 0x000000;
 	
 	public static void datagenInit() {
-		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, CLEARING_ID.getValue().toString(), createClearing(false, new SpawnSettings.Builder()));
-		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, OVERGROWN_CLEARING_ID.getValue().toString(), createClearing(true, new SpawnSettings.Builder().spawn(SpawnGroup.AMBIENT, new SpawnSettings.SpawnEntry(ModEntityTypes.PILOT_LIGHT, 1, 1, 1)).creatureSpawnProbability(0.1F)));
+		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, CLEARING_ID.getValue().toString(), createClearing(false, SpawnSettingsBuilder.none()));
+		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, OVERGROWN_CLEARING_ID.getValue().toString(), createClearing(true, SpawnSettingsBuilder.builder().ambient(ModEntityTypes.PILOT_LIGHT, 1, 1, 1).spawnChance(0.1F).build()));
 		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, COPSE_ID.getValue().toString(), createForest(false, COPSE_MOBS));
 		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, DEEPWOOD_ID.getValue().toString(), createForest(true, DEEPWOOD_MOBS));
 		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, CONIFEROUS_COPSE_ID.getValue().toString(), createConiferousForest(false, COPSE_MOBS));
 		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, CONIFEROUS_DEEPWOOD_ID.getValue().toString(), createConiferousForest(true, DEEPWOOD_MOBS));
-		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, UPLANDS_ID.getValue().toString(), createUplands(new SpawnSettings.Builder()));
+		BuiltinRegistries.addCasted(BuiltinRegistries.BIOME, UPLANDS_ID.getValue().toString(), createUplands(SpawnSettingsBuilder.none()));
 	}
 	
-	private static Biome createClearing(boolean overgrown, SpawnSettings.Builder spawnSettings) {
-		GenerationSettings.Builder builder = new GenerationSettings.Builder();
-		addLandCarversNotLavaLakes(builder);
-		addBasicVanillaOres(builder);
-//		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.CLEARING_FLOWERS);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_GRASS_PLAIN);
-		addMarigolds(builder);
-		if (overgrown) {
-			builder.feature(GenerationStep.Feature.LAKES, ModPlacedFeatures.SPRING);
-			builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModVegetationFeatures.OVERGROWTH_CLEARING_TREES_PLACED);
-			builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.BUSHES);
-			builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_TALL_GRASS);
-			addStrewnLeaves(builder);
-		}
-		addWaterSprings(builder);
-		DefaultBiomeFeatures.addFrozenTopLayer(builder);
-		BiomeEffects.Builder effects = new BiomeEffects.Builder().foliageColor(AYLYTHIAN_FOLIAGE_COLOR).grassColor(overgrown ? 0xBC953A : 0xA1BA48).waterColor(WATER_COLOR).waterFogColor(UNDERWATER_COLOR).fogColor(FOG_COLOR).skyColor(SKY_COLOR).moodSound(BiomeMoodSound.CAVE).particleConfig(new BiomeParticleConfig(ModParticles.AMBIENT_PILOT_LIGHT, 0.0025F));
-		if (overgrown) {
-			effects = effects.additionsSound(OVERGROWN_CLEARING_AMBIANCE);
-		}
-		return new Biome.Builder().precipitation(Biome.Precipitation.RAIN).temperature(0.7F).downfall(0.8F).effects(effects.build()).spawnSettings(spawnSettings.build()).generationSettings(builder.build()).build();
+	private static Biome createClearing(boolean overgrown, SpawnSettings spawnSettings) {
+		return BiomeBuilder.builder(Biome.Precipitation.RAIN, 0.7F, 0.8F)
+				.biomeEffects(FOG_COLOR, WATER_COLOR, UNDERWATER_COLOR, SKY_COLOR, biomeEffectsBuilder -> {
+					biomeEffectsBuilder.foliageColor(AYLYTHIAN_FOLIAGE_COLOR)
+							.grassColor(overgrown ? 0xBC953A : 0xA1BA48)
+							.moodSound(BiomeMoodSound.CAVE)
+							.particleConfig(ModParticles.AMBIENT_PILOT_LIGHT, 0.0025F);
+					if (overgrown) {
+						biomeEffectsBuilder.additionsSound(OVERGROWN_CLEARING_AMBIANCE);
+					}
+				})
+				.spawnSettings(spawnSettings)
+				.generationSettings(generationSettingsBuilder -> {
+					generationSettingsBuilder
+							.add(ModBiomes::addLandCarversNotLavaLakes)
+							.add(ModBiomes::addBasicVanillaOres)
+							.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_GRASS_PLAIN)
+							.add(ModBiomes::addMarigolds);
+					if (overgrown) {
+						generationSettingsBuilder.lakesFeature(ModPlacedFeatures.SPRING)
+								.vegetalDecoFeature(ModVegetationFeatures.OVERGROWTH_CLEARING_TREES_PLACED)
+								.vegetalDecoFeature(ModPlacedFeatures.BUSHES)
+								.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_TALL_GRASS)
+								.add(ModBiomes::addStrewnLeaves);
+					}
+					generationSettingsBuilder.add(ModBiomes::addWaterSprings)
+							.add(DefaultBiomeFeatures::addFrozenTopLayer);
+				}).build();
 	}
 	
-	private static Biome createForest(boolean deep, SpawnSettings.Builder spawnSettings) {
-		GenerationSettings.Builder builder = new GenerationSettings.Builder();
-		addLandCarversNotLavaLakes(builder);
-		addBasicVanillaOres(builder);
-		builder.feature(GenerationStep.Feature.LAKES, ModPlacedFeatures.SPRING);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, deep ? ModVegetationFeatures.DEEP_ROOF_TREES_PLACED : ModPlacedFeatures.AYLYTHIAN_DARK_OAK);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, deep ? ModVegetationFeatures.DEEPWOOD_TREES_PLACED : ModVegetationFeatures.COPSE_TREES_PLACED);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_GRASS_TAIGA);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.BUSHES);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_TALL_GRASS);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.AYLYTH_WEEDS);
-		if (deep) {
-			builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_TALL_GRASS_2);
-			addLeafPiles(builder);
-			DefaultBiomeFeatures.addLargeFerns(builder);
-		}
-		DefaultBiomeFeatures.addDefaultMushrooms(builder);
-		addStrewnLeaves(builder);
-		addWaterSprings(builder);
-		DefaultBiomeFeatures.addFrozenTopLayer(builder);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.YMPE_SEEP);
-		return new Biome.Builder().precipitation(Biome.Precipitation.RAIN).temperature(0.7F).downfall(0.8F).effects(new BiomeEffects.Builder().foliageColor(deep ? DEEP_AYLYTHIAN_FOLIAGE_COLOR : AYLYTHIAN_FOLIAGE_COLOR).grassColor(deep ? 0xAD6903 : 0xB5883B).waterColor(WATER_COLOR).waterFogColor(UNDERWATER_COLOR).fogColor(FOG_COLOR).skyColor(SKY_COLOR).moodSound(BiomeMoodSound.CAVE).particleConfig(new BiomeParticleConfig(ParticleTypes.MYCELIUM, deep ? 0.1F : 0.025F)).additionsSound(FOREST_AMBIANCE).build()).spawnSettings(spawnSettings.build()).generationSettings(builder.build()).build();
+	private static Biome createForest(boolean deep, SpawnSettings spawnSettings) {
+		return BiomeBuilder.builder(Biome.Precipitation.RAIN, 0.7F, 0.8F)
+				.biomeEffects(FOG_COLOR, WATER_COLOR, UNDERWATER_COLOR, SKY_COLOR, biomeEffectsBuilder -> {
+					biomeEffectsBuilder.foliageColor(deep ? DEEP_AYLYTHIAN_FOLIAGE_COLOR : AYLYTHIAN_FOLIAGE_COLOR)
+							.grassColor(deep ? 0xAD6903 : 0xB5883B)
+							.moodSound(BiomeMoodSound.CAVE)
+							.particleConfig(ParticleTypes.MYCELIUM, deep ? 0.1F : 0.025F)
+							.additionsSound(FOREST_AMBIANCE);
+				})
+				.spawnSettings(spawnSettings)
+				.generationSettings(generationSettingsBuilder -> {
+					generationSettingsBuilder.add(ModBiomes::addLandCarversNotLavaLakes)
+							.add(ModBiomes::addBasicVanillaOres)
+							.lakesFeature(ModPlacedFeatures.SPRING)
+							.vegetalDecoFeature(deep ? ModVegetationFeatures.DEEP_ROOF_TREES_PLACED : ModPlacedFeatures.AYLYTHIAN_DARK_OAK)
+							.vegetalDecoFeature(deep ? ModVegetationFeatures.DEEPWOOD_TREES_PLACED : ModVegetationFeatures.COPSE_TREES_PLACED)
+							.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_GRASS_TAIGA)
+							.vegetalDecoFeature(ModPlacedFeatures.BUSHES)
+							.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_TALL_GRASS)
+							.vegetalDecoFeature(ModPlacedFeatures.AYLYTH_WEEDS);
+					if (deep) {
+						generationSettingsBuilder.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_TALL_GRASS_2)
+								.add(ModBiomes::addLeafPiles)
+								.add(DefaultBiomeFeatures::addLargeFerns);
+					}
+					generationSettingsBuilder.add(DefaultBiomeFeatures::addDefaultMushrooms)
+							.add(ModBiomes::addStrewnLeaves)
+							.add(ModBiomes::addWaterSprings)
+							.add(DefaultBiomeFeatures::addFrozenTopLayer)
+							.vegetalDecoFeature(ModPlacedFeatures.YMPE_SEEP);
+				}).build();
 	}
 	
-	private static Biome createConiferousForest(boolean deep, SpawnSettings.Builder spawnSettings) {
-		GenerationSettings.Builder builder = new GenerationSettings.Builder();
-		addLandCarversNotLavaLakes(builder);
-		addBasicVanillaOres(builder);
-		DefaultBiomeFeatures.addForestFlowers(builder);
-		builder.feature(GenerationStep.Feature.LAKES, ModPlacedFeatures.SPRING);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, VegetationPlacedFeatures.PATCH_GRASS_TAIGA);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.AYLYTH_WEEDS);
-		if (deep) {
-			builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModVegetationFeatures.CONIFEROUS_DEEP_ROOF_TREES_PLACED);
-			addLeafPiles(builder);
-			DefaultBiomeFeatures.addLargeFerns(builder);
-		}
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, deep ? ModVegetationFeatures.CONIFEROUS_DEEPWOOD_TREES_PLACED : ModVegetationFeatures.CONIFEROUS_COPSE_TREES_PLACED);
-		DefaultBiomeFeatures.addDefaultMushrooms(builder);
-		addStrewnLeaves(builder);
-		addWaterSprings(builder);
-		DefaultBiomeFeatures.addFrozenTopLayer(builder);
-		builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.YMPE_SEEP);
-		return new Biome.Builder().precipitation(Biome.Precipitation.RAIN).temperature(0.7F).downfall(0.8F).effects(new BiomeEffects.Builder().foliageColor(deep ? DEEP_AYLYTHIAN_FOLIAGE_COLOR : AYLYTHIAN_FOLIAGE_COLOR).grassColor(deep ? 0x3E682B : 0x4D7C44).waterColor(WATER_COLOR).waterFogColor(UNDERWATER_COLOR).fogColor(FOG_COLOR).skyColor(SKY_COLOR).moodSound(BiomeMoodSound.CAVE).particleConfig(new BiomeParticleConfig(ParticleTypes.MYCELIUM, deep ? 0.1F : 0.025F)).additionsSound(FOREST_AMBIANCE).build()).spawnSettings(spawnSettings.build()).generationSettings(builder.build()).build();
+	private static Biome createConiferousForest(boolean deep, SpawnSettings spawnSettings) {
+		return BiomeBuilder.builder(Biome.Precipitation.RAIN, 0.7F, 0.8F)
+				.biomeEffects(FOG_COLOR, WATER_COLOR, UNDERWATER_COLOR, SKY_COLOR, biomeEffectsBuilder -> {
+					biomeEffectsBuilder.foliageColor(deep ? DEEP_AYLYTHIAN_FOLIAGE_COLOR : AYLYTHIAN_FOLIAGE_COLOR)
+							.grassColor(deep ? 0x3E682B : 0x4D7C44)
+							.moodSound(BiomeMoodSound.CAVE)
+							.particleConfig(ParticleTypes.MYCELIUM, deep ? 0.1F : 0.025F)
+							.additionsSound(FOREST_AMBIANCE);
+				})
+				.spawnSettings(spawnSettings)
+				.generationSettings(builder -> {
+					builder.add(ModBiomes::addLandCarversNotLavaLakes)
+							.add(ModBiomes::addBasicVanillaOres)
+							.add(DefaultBiomeFeatures::addForestFlowers)
+							.lakesFeature(ModPlacedFeatures.SPRING)
+							.vegetalDecoFeature(VegetationPlacedFeatures.PATCH_GRASS_TAIGA)
+							.vegetalDecoFeature(ModPlacedFeatures.AYLYTH_WEEDS);
+					if (deep) {
+						builder.vegetalDecoFeature(ModVegetationFeatures.CONIFEROUS_DEEP_ROOF_TREES_PLACED)
+								.add(ModBiomes::addLeafPiles)
+								.add(DefaultBiomeFeatures::addLargeFerns);
+					}
+					builder.vegetalDecoFeature(deep ? ModVegetationFeatures.CONIFEROUS_DEEPWOOD_TREES_PLACED : ModVegetationFeatures.CONIFEROUS_COPSE_TREES_PLACED)
+							.add(DefaultBiomeFeatures::addDefaultMushrooms)
+							.add(ModBiomes::addStrewnLeaves)
+							.add(ModBiomes::addWaterSprings)
+							.add(DefaultBiomeFeatures::addFrozenTopLayer)
+							.vegetalDecoFeature(ModPlacedFeatures.YMPE_SEEP);
+				}).build();
 	}
 
-	private static Biome createUplands(SpawnSettings.Builder spawnSettings) {
-		GenerationSettings.Builder builder = new GenerationSettings.Builder();
-		addLandCarversNotLavaLakes(builder);
-		addBasicVanillaOres(builder);
-		addWaterSprings(builder);
-		return new Biome.Builder().precipitation(Biome.Precipitation.NONE).temperature(0.8f).downfall(0.3f).effects(new BiomeEffects.Builder().foliageColor(AYLYTHIAN_FOLIAGE_COLOR).grassColor(0xB5883B).waterColor(WATER_COLOR).waterFogColor(UNDERWATER_COLOR).fogColor(FOG_COLOR).skyColor(SKY_COLOR).moodSound(BiomeMoodSound.CAVE).build()).spawnSettings(spawnSettings.build()).generationSettings(builder.build()).build();
+	private static Biome createUplands(SpawnSettings spawnSettings) {
+		return BiomeBuilder.builder(Biome.Precipitation.NONE, 0.8F, 0.3F)
+				.biomeEffects(FOG_COLOR, WATER_COLOR, UNDERWATER_COLOR, SKY_COLOR, biomeEffectsBuilder -> {
+					biomeEffectsBuilder.foliageColor(AYLYTHIAN_FOLIAGE_COLOR)
+							.grassColor(0xB5883B)
+							.moodSound(BiomeMoodSound.CAVE);
+				})
+				.spawnSettings(spawnSettings)
+				.generationSettings(builder -> {
+					builder.add(ModBiomes::addLandCarversNotLavaLakes)
+							.add(ModBiomes::addBasicVanillaOres)
+							.add(ModBiomes::addWaterSprings);
+				}).build();
 	}
 
 	private static void addLandCarversNotLavaLakes(GenerationSettings.Builder builder) {
