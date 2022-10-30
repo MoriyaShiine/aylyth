98d1c315af5a5a7afa98e972a23af752b3a1264b
dhyces <pokmon987@gmail.com>
2022-10-30 15:34:11 -0400

    Update WyrdedStatusEffect.java

--- src/main/java/moriyashiine/aylyth/common/statuseffect/WyrdedStatusEffect.java
+++ src/main/java/moriyashiine/aylyth/common/statuseffect/WyrdedStatusEffect.java
@@ -3,36 +3,34 @@ package moriyashiine.aylyth.common.statuseffect;
 import moriyashiine.aylyth.common.AylythUtil;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
-import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 
 import java.util.UUID;
 
 public class WyrdedStatusEffect extends StatusEffect {
 
+    private static final UUID MODIFIER_UUID = UUID.fromString("55683eb4-97eb-41a0-8b6a-3ffaf5dfb21b");
+
     public WyrdedStatusEffect() {
         super(StatusEffectCategory.HARMFUL, 0x695237);
-        addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "55683eb4-97eb-41a0-8b6a-3ffaf5dfb21b", 1.0, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         var instance = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        var modifier = getAttributeModifiers().get(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        if (!AylythUtil.isNearSeep(entity, 5)) {
-            if (instance != null && instance.hasModifier(modifier)) {
-                instance.removeModifier(modifier);
+        var distance = AylythUtil.distanceIfNearSeep(entity, 5);
+        if (distance == -1) {
+            if (instance != null) {
+                instance.tryRemoveModifier(MODIFIER_UUID);
             }
         } else {
-            entity.damage(DamageSource.MAGIC, 0.5f + ((float)amplifier * 0.5f));
-            if (instance != null && !instance.hasModifier(modifier)) {
-                instance.addPersistentModifier(new EntityAttributeModifier(modifier.getId(), this::getTranslationKey, -(0.45 + (Math.sqrt(amplifier) / 10)), EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+            entity.damage(DamageSource.MAGIC, 2f + ((float)amplifier * 2));
+            if (instance != null && (instance.tryRemoveModifier(MODIFIER_UUID) || instance.getModifier(MODIFIER_UUID) == null)) {
+                instance.addPersistentModifier(new EntityAttributeModifier(MODIFIER_UUID, this::getTranslationKey, -(0.45 + (Math.sqrt(amplifier) / 10) + ((5.0 - distance) / 12.5)), EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
             }
         }
     }
@@ -40,6 +38,11 @@ public class WyrdedStatusEffect extends StatusEffect {
     @Override
     public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
         // NO-OP
+    }
+
+    @Override
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).tryRemoveModifier(MODIFIER_UUID);
     }
 
     @Override
