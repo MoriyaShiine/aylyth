7911d9c5bf6aae617ce79b7df73b9de9b6758d2b
dhyces <pokmon987@gmail.com>
2022-10-30 15:38:32 -0400

    Datagen loot table and recipes

--- src/main/java/moriyashiine/aylyth/datagen/AylythRecipeProvider.java
+++ src/main/java/moriyashiine/aylyth/datagen/AylythRecipeProvider.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
@@ -28,6 +29,7 @@ public class AylythRecipeProvider extends FabricRecipeProvider {
         offerSingleOutputShapelessRecipe(exporter, Items.ORANGE_DYE, ModItems.MARIGOLD, "");
         createTwoByTwo(exporter, Items.SHROOMLIGHT, 1, ModItems.JACK_O_LANTERN_MUSHROOM, "shroomlight_from_jack_o_lantern_mushroom");
         woodSuiteRecipes(exporter, ModItems.POMEGRANATE_ITEMS);
+        offerShapeless(exporter, ModItems.GHOSTCAP_MUSHROOM_SPORES, 1, ModItems.GHOSTCAP_MUSHROOM, null, "ghostcap_spores_from_ghostcap_mushroom");
     }
 
     private void woodSuiteRecipes(Consumer<RecipeJsonProvider> exporter, ItemWoodSuite suite) {
@@ -47,12 +49,24 @@ public class AylythRecipeProvider extends FabricRecipeProvider {
         offerChestBoatRecipe(exporter, suite.chestBoat, suite.planks);
     }
 
-    private void createTwoByTwo(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, int outputCount, ItemConvertible input, String fileId) {
-        ShapedRecipeJsonBuilder.create(output, outputCount).input('#', input).pattern("## ").pattern("## ").criterion(RecipeProvider.hasItem(input), conditionsFromItem(input)).offerTo(exporter, fileId);
+    private void createTwoByTwo(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, int outputCount, ItemConvertible input, String recipeId) {
+        ShapedRecipeJsonBuilder.create(output, outputCount).input('#', input).pattern("## ").pattern("## ").criterion(RecipeProvider.hasItem(input), conditionsFromItem(input)).offerTo(exporter, recipeId);
     }
 
-    private void createTwoByTwo(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, int outputCount, ItemConvertible input, String group, String fileId) {
-        ShapedRecipeJsonBuilder.create(output, outputCount).input('#', input).pattern("## ").pattern("## ").group(group).criterion(RecipeProvider.hasItem(input), conditionsFromItem(input)).offerTo(exporter, fileId);
+    private void createTwoByTwo(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, int outputCount, ItemConvertible input, String group, String recipeId) {
+        ShapedRecipeJsonBuilder.create(output, outputCount).input('#', input).pattern("## ").pattern("## ").group(group).criterion(RecipeProvider.hasItem(input), conditionsFromItem(input)).offerTo(exporter, recipeId);
+    }
+
+    private void offerShapeless(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, int outputCount, ItemConvertible input, @Nullable String group, @Nullable String recipeId) {
+        var recipe = shapeless(output, outputCount).input(input).criterion(RecipeProvider.hasItem(input), conditionsFromItem(input));
+        if (group != null) {
+            recipe.group(group);
+        }
+        if (recipeId != null) {
+            recipe.offerTo(exporter, new Identifier(dataGenerator.getModId(), recipeId));
+        } else {
+            recipe.offerTo(exporter);
+        }
     }
 
     private ShapelessRecipeJsonBuilder shapeless(ItemConvertible output, int outputCount) {
