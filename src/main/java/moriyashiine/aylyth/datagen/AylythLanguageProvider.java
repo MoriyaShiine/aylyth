03789f5afa30bb4e7d10859d673ee1bd1e2cd6ef
dhyces <pokmon987@gmail.com>
2022-10-30 19:07:23 -0400

    Add more subtitles to datagen

--- src/main/java/moriyashiine/aylyth/datagen/AylythLanguageProvider.java
+++ src/main/java/moriyashiine/aylyth/datagen/AylythLanguageProvider.java
@@ -1,6 +1,9 @@
 package moriyashiine.aylyth.datagen;
 
-import moriyashiine.aylyth.common.registry.*;
+import moriyashiine.aylyth.common.registry.ModBlocks;
+import moriyashiine.aylyth.common.registry.ModEntityTypes;
+import moriyashiine.aylyth.common.registry.ModItems;
+import moriyashiine.aylyth.common.registry.ModPotions;
 import moriyashiine.aylyth.common.registry.util.WoodSuite;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
@@ -16,88 +19,93 @@ public class AylythLanguageProvider extends FabricLanguageProvider {
 
     @Override
     public void generateTranslations(TranslationBuilder builder) {
-            // MOD ITEMGROUP
+        // MOD ITEMGROUP
 
-            builder.add(ModItems.GROUP, "Aylyth");
+        builder.add(ModItems.GROUP, "Aylyth");
 
-            // BLOCKS
+        // BLOCKS
 
-            String ympe = "Ympe";
-            addWoodsuite(builder, ModBlocks.YMPE_BLOCKS, ympe);
-            builder.add(ModBlocks.FRUIT_BEARING_YMPE_LOG, "Fruit-bearing Ympe Log");
-            leaves(builder, ModBlocks.YMPE_LEAVES, ympe);
-            String pomegranate = "Pomegranate";
-            addWoodsuite(builder, ModBlocks.POMEGRANATE_BLOCKS, pomegranate);
-            leaves(builder, ModBlocks.POMEGRANATE_LEAVES, pomegranate);
-            builder.add(ModBlocks.AYLYTH_BUSH, "Aylyth Bush");
-            builder.add(ModBlocks.ANTLER_SHOOTS, "Antler Shoots");
-            builder.add(ModBlocks.GRIPWEED, "Gripweed");
-            builder.add(ModBlocks.NYSIAN_GRAPE_VINE, "Nysian Grape Vines");
-            builder.add(ModBlocks.MARIGOLD, "Marigolds");
-            builder.add(ModBlocks.OAK_STREWN_LEAVES, "Oak Strewn Leaves");
-            builder.add(ModBlocks.YMPE_STREWN_LEAVES, "Ympe Strewn Leaves");
-            builder.add(ModBlocks.JACK_O_LANTERN_MUSHROOM, "Jack O'Lantern Mushroom");
-            builder.add(ModBlocks.GHOSTCAP_MUSHROOM, "Ghostcap Mushroom Spores");
-            builder.add(ModBlocks.OAK_SEEP, "Oak Seep");
-            builder.add(ModBlocks.SPRUCE_SEEP, "Spruce Seep");
-            builder.add(ModBlocks.DARK_OAK_SEEP, "Dark Oak Seep");
-            builder.add(ModBlocks.YMPE_SEEP, "Ympe Seep");
+        String ympe = "Ympe";
+        addWoodsuite(builder, ModBlocks.YMPE_BLOCKS, ympe);
+        builder.add(ModBlocks.FRUIT_BEARING_YMPE_LOG, "Fruit Bearing Ympe Log");
+        leaves(builder, ModBlocks.YMPE_LEAVES, ympe);
+        String pomegranate = "Pomegranate";
+        addWoodsuite(builder, ModBlocks.POMEGRANATE_BLOCKS, pomegranate);
+        leaves(builder, ModBlocks.POMEGRANATE_LEAVES, pomegranate);
+        builder.add(ModBlocks.AYLYTH_BUSH, "Aylyth Bush");
+        builder.add(ModBlocks.ANTLER_SHOOTS, "Antler Shoots");
+        builder.add(ModBlocks.GRIPWEED, "Gripweed");
+        builder.add(ModBlocks.NYSIAN_GRAPE_VINE, "Nysian Grape Vines");
+        builder.add(ModBlocks.MARIGOLD, "Marigolds");
+        builder.add(ModBlocks.OAK_STREWN_LEAVES, "Oak Strewn Leaves");
+        builder.add(ModBlocks.YMPE_STREWN_LEAVES, "Ympe Strewn Leaves");
+        builder.add(ModBlocks.JACK_O_LANTERN_MUSHROOM, "Jack O'Lantern Mushroom");
+        builder.add(ModBlocks.GHOSTCAP_MUSHROOM, "Ghostcap Mushroom Spores");
+        builder.add(ModBlocks.OAK_SEEP, "Oak Seep");
+        builder.add(ModBlocks.SPRUCE_SEEP, "Spruce Seep");
+        builder.add(ModBlocks.DARK_OAK_SEEP, "Dark Oak Seep");
+        builder.add(ModBlocks.YMPE_SEEP, "Ympe Seep");
 
-            // ITEMS
+        // ITEMS
 
-            boat(builder, ModItems.YMPE_ITEMS.boat, ympe);
-            chestBoat(builder, ModItems.YMPE_ITEMS.chestBoat, ympe);
-            boat(builder, ModItems.POMEGRANATE_ITEMS.boat, pomegranate);
-            chestBoat(builder, ModItems.POMEGRANATE_ITEMS.chestBoat, pomegranate);
-            builder.add(ModItems.POMEGRANATE, pomegranate);
-            builder.add(ModItems.GHOSTCAP_MUSHROOM, "Ghostcap Mushroom");
-            builder.add(ModItems.YMPE_DAGGER, "Ympe Dagger");
-            builder.add(ModItems.YMPE_LANCE, "Ympe Lance");
-            builder.add(ModItems.YMPE_FRUIT, "Ympe Fruit");
-            builder.add(ModItems.SHUCKED_YMPE_FRUIT, "Shucked Ympe Fruit");
-            builder.add(ModItems.NYSIAN_GRAPES, "Nysian Grapes");
-            builder.add(ModItems.CORIC_SEED, "Coric Seed");
-            builder.add(ModItems.AYLYTHIAN_HEART, "Aylythian Heart");
-            builder.add(ModItems.PILOT_LIGHT_SPAWN_EGG, "Pilot Light Spawn Egg");
-            builder.add(ModItems.AYLYTHIAN_SPAWN_EGG, "Aylythian Spawn Egg");
-            builder.add(ModItems.ELDER_AYLYTHIAN_SPAWN_EGG, "Elder Aylythian Spawn Egg");
-            potionSet(builder, "mortechis");
-            potionSet(builder, "cimmerian");
-            potionSet(builder, "wyrded");
+        boat(builder, ModItems.YMPE_ITEMS.boat, ympe);
+        chestBoat(builder, ModItems.YMPE_ITEMS.chestBoat, ympe);
+        boat(builder, ModItems.POMEGRANATE_ITEMS.boat, pomegranate);
+        chestBoat(builder, ModItems.POMEGRANATE_ITEMS.chestBoat, pomegranate);
+        builder.add(ModItems.POMEGRANATE, pomegranate);
+        builder.add(ModItems.GHOSTCAP_MUSHROOM, "Ghostcap Mushroom");
+        builder.add(ModItems.YMPE_DAGGER, "Ympe Dagger");
+        builder.add(ModItems.YMPE_LANCE, "Ympe Lance");
+        builder.add(ModItems.YMPE_FRUIT, "Ympe Fruit");
+        builder.add(ModItems.SHUCKED_YMPE_FRUIT, "Shucked Ympe Fruit");
+        builder.add(ModItems.NYSIAN_GRAPES, "Nysian Grapes");
+        builder.add(ModItems.CORIC_SEED, "Coric Seed");
+        builder.add(ModItems.AYLYTHIAN_HEART, "Aylythian Heart");
+        builder.add(ModItems.PILOT_LIGHT_SPAWN_EGG, "Pilot Light Spawn Egg");
+        builder.add(ModItems.AYLYTHIAN_SPAWN_EGG, "Aylythian Spawn Egg");
+        builder.add(ModItems.ELDER_AYLYTHIAN_SPAWN_EGG, "Elder Aylythian Spawn Egg");
+        potionSet(builder, "mortechis");
+        potionSet(builder, "cimmerian");
+        potionSet(builder, "wyrded");
 
-            // STATUS EFFECTS
+        // STATUS EFFECTS
 
-            builder.add(ModPotions.MORTECHIS_EFFECT, "Mortechis");
-            builder.add(ModPotions.CIMMERIAN_EFFECT, "Cimmerian");
-            builder.add(ModPotions.WYRDED_EFFECT, "Wyrded");
+        builder.add(ModPotions.MORTECHIS_EFFECT, "Mortechis");
+        builder.add(ModPotions.CIMMERIAN_EFFECT, "Cimmerian");
+        builder.add(ModPotions.WYRDED_EFFECT, "Wyrded");
 
-            // ENTITIES
+        // ENTITIES
 
-            builder.add(ModEntityTypes.PILOT_LIGHT, "Pilot Light");
-            builder.add(ModEntityTypes.AYLYTHIAN, "Aylythian");
-            builder.add(ModEntityTypes.ELDER_AYLYTHIAN, "Elder Aylythian");
-            builder.add(ModEntityTypes.YMPE_LANCE, "Ympe Lance");
+        builder.add(ModEntityTypes.PILOT_LIGHT, "Pilot Light");
+        builder.add(ModEntityTypes.AYLYTHIAN, "Aylythian");
+        builder.add(ModEntityTypes.ELDER_AYLYTHIAN, "Elder Aylythian");
+        builder.add(ModEntityTypes.YMPE_LANCE, "Ympe Lance");
 
-            // DEATH MESSAGES
+        // DEATH MESSAGES
 
-            builder.add("death.attack.ympe", "%1$s has gone to the trees");
-            builder.add("death.attack.ympe.player", "%1$s has gone to the trees whilst trying to escape %2$s");
+        builder.add("death.attack.ympe", "%1$s has gone to the trees");
+        builder.add("death.attack.ympe.player", "%1$s has gone to the trees whilst trying to escape %2$s");
 
-            // SUBTITLES
+        // SUBTITLES
 
-            builder.add("aylyth.subtitles.block.ympe_log.pick_fruit", "Fruit picked");
-            builder.add("aylyth.subtitles.entity.player.increase_ympe_infestation_stage", "Branches spread");
-            builder.add("aylyth.subtitles.entity.generic.shucked", "Entity shucked");
-            builder.add("aylyth.subtitles.entity.aylythian.ambient", "Aylythian groans");
-            builder.add("aylyth.subtitles.entity.aylythian.hurt", "Aylythian hurts");
-            builder.add("aylyth.subtitles.entity.aylythian.death", "Aylythian dies");
-            builder.add("aylyth.subtitles.entity.elder_aylythian.ambient", "Elder Aylythian rumbles");
-            builder.add("aylyth.subtitles.entity.elder_aylythian.hurt", "Elder Aylythian hurts");
-            builder.add("aylyth.subtitles.entity.elder_aylythian.death", "Elder Aylythian dies");
+        builder.add("aylyth.subtitles.block.ympe_log.pick_fruit", "Fruit picked");
+        builder.add("aylyth.subtitles.entity.player.increase_ympe_infestation_stage", "Branches spread");
+        builder.add("aylyth.subtitles.entity.generic.shucked", "Entity shucked");
+        builder.add("aylyth.subtitles.entity.aylythian.ambient", "Aylythian groans");
+        builder.add("aylyth.subtitles.entity.aylythian.hurt", "Aylythian hurts");
+        builder.add("aylyth.subtitles.entity.aylythian.death", "Aylythian dies");
+        builder.add("aylyth.subtitles.entity.elder_aylythian.ambient", "Elder Aylythian rumbles");
+        builder.add("aylyth.subtitles.entity.elder_aylythian.hurt", "Elder Aylythian hurts");
+        builder.add("aylyth.subtitles.entity.elder_aylythian.death", "Elder Aylythian dies");
+        builder.add("aylyth.subtitles.block.stick_break", "Stick cracks");
+        builder.add("aylyth.subtitles.block.strewn_leaves.step", "Leaves crunch");
+        builder.add("aylyth.subtitles.block.strewn_leaves.pile_destroy", "Leaves scatter");
+        builder.add("aylyth.subtitles.block.strewn_leaves.pile_step", "Leaf pile crunches");
 
-            // COMPAT - REI
 
-            builder.add("rei.aylyth.ympe_dagger_drops", "Ympe Dagger Drops");
+        // COMPAT - REI
+
+        builder.add("rei.aylyth.ympe_dagger_drops", "Ympe Dagger Drops");
     }
 
     private void potionSet(TranslationBuilder builder, String effectName) {
