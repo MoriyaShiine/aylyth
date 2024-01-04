d359cfb4a353c76b7bee11c96aa331d4d780bc6f
dhyces <10985914+dhyces@users.noreply.github.com>
2024-01-03 18:07:42 -0800

    Mild cleanup
    Use a temporary modifier for tick, since it'll be applied every tick

--- src/main/java/moriyashiine/aylyth/common/statuseffect/CimmerianStatusEffect.java
+++ src/main/java/moriyashiine/aylyth/common/statuseffect/CimmerianStatusEffect.java
@@ -3,7 +3,6 @@ package moriyashiine.aylyth.common.statuseffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
-// TODO: finish impl
 public class CimmerianStatusEffect extends StatusEffect {
     public CimmerianStatusEffect() {
         super(StatusEffectCategory.BENEFICIAL, 0xFFAABB);
