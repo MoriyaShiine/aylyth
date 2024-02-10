92390297d6a121be05638a8f5cc3f325f2453f52
dhyces <10985914+dhyces@users.noreply.github.com>
2024-02-09 21:35:59 -0500

    In case anyone ever depends on this ig

--- src/main/java/moriyashiine/aylyth/mixin/MilkBucketItemMixin.java
+++ src/main/java/moriyashiine/aylyth/mixin/MilkBucketItemMixin.java
@@ -2,39 +2,36 @@ package moriyashiine.aylyth.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import moriyashiine.aylyth.common.registry.tag.ModEffectTags;
 import net.fabricmc.fabric.api.tag.convention.v1.TagUtil;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.MilkBucketItem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(MilkBucketItem.class)
 public class MilkBucketItemMixin {
     @WrapOperation(method = "finishUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;clearStatusEffects()Z"))
     private boolean finishUsing(LivingEntity player, Operation<Boolean> original) {
 
-        ArrayList<StatusEffectInstance> arr = new ArrayList<>();
+        List<StatusEffectInstance> arr = new ObjectArrayList<>();
         for (StatusEffectInstance persistentStatusEffect : player.getStatusEffects()) {
             if (TagUtil.isIn(ModEffectTags.MILK_BYPASSING, persistentStatusEffect.getEffectType())) {
                 arr.add(persistentStatusEffect);
             }
         }
 
-        original.call(player);
+        boolean originalReturn = original.call(player);
 
         for (StatusEffectInstance effectInstance : arr) {
             player.addStatusEffect(effectInstance);
         }
 
-        return true;
+        return originalReturn;
     }
 }
