d56f18d05b5eb2f3e82ec288c365d289544220f0
dhyces <10985914+dhyces@users.noreply.github.com>
2024-02-10 15:33:56 -0500

    Use JetBrains annotations

--- src/main/java/moriyashiine/aylyth/common/world/ModDamageSources.java
+++ src/main/java/moriyashiine/aylyth/common/world/ModDamageSources.java
@@ -1,15 +1,15 @@
 package moriyashiine.aylyth.common.world;
 
-import blue.endless.jankson.annotation.Nullable;
 import moriyashiine.aylyth.common.entity.mob.WreathedHindEntity;
 import moriyashiine.aylyth.common.registry.key.ModDamageTypeKeys;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
+import org.jetbrains.annotations.Nullable;
 
 public class ModDamageSources {
 	private final Registry<DamageType> damageTypeRegistry;
@@ -22,8 +22,8 @@ public class ModDamageSources {
 		return new DamageSource(damageTypeRegistry.entryOf(ModDamageTypeKeys.YMPE));
 	}
 
-	public DamageSource ympeEntity() {
-		return new DamageSource(damageTypeRegistry.entryOf(ModDamageTypeKeys.YMPE_ENTITY));
+	public DamageSource ympeEntity(Entity entity) {
+		return new DamageSource(damageTypeRegistry.entryOf(ModDamageTypeKeys.YMPE_ENTITY), entity);
 	}
 
 	public DamageSource killingBlow(WreathedHindEntity wreathedHindEntity) {
@@ -37,35 +37,4 @@ public class ModDamageSources {
 	public DamageSource blight(@Nullable PlayerEntity player) {
 		return new DamageSource(damageTypeRegistry.entryOf(ModDamageTypeKeys.BLIGHT), player);
 	}
-
-	/*
-	public static float handleDamage(LivingEntity livingEntity, DamageSource source, float amount) {
-		if(livingEntity instanceof PlayerEntity player){
-			CuirassComponent component = ModComponents.CUIRASS_COMPONENT.get(player);
-			boolean bl = source.isMagic() || source.isFromFalling() || source.isOutOfWorld();
-			boolean bl2 = source.getAttacker() != null && source.getAttacker() instanceof LivingEntity livingEntity1 && livingEntity1.getMainHandStack().getItem() instanceof AxeItem;
-			boolean bl3 = source.isFire();
-			if(bl2 || bl3){
-				component.setStage(0);
-				component.setStageTimer(0);
-				player.world.playSoundFromEntity(null, player, ModSoundEvents.ENTITY_PLAYER_INCREASE_YMPE_INFESTATION_STAGE, SoundCategory.PLAYERS, 1, player.getSoundPitch());
-				System.out.println("isFire");
-				return amount;
-			} else if(bl){
-				System.out.println("isMagic");
-				return amount;
-			} else {
-				while (component.getStage() > 0) {
-					System.out.println("while");
-					amount--;
-					component.setStage(component.getStage() - 1);
-					player.world.playSoundFromEntity(null, player, ModSoundEvents.ENTITY_PLAYER_INCREASE_YMPE_INFESTATION_STAGE, SoundCategory.PLAYERS, 1, player.getSoundPitch());
-				}
-				return amount;
-			}
-		}
-		return amount;
-	}
-
-	 */
 }
