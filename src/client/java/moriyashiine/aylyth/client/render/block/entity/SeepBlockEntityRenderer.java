17b83bf4bd63c1dcff456690d0448e2f89272feb
dhyces <10985914+dhyces@users.noreply.github.com>
2024-02-21 00:33:27 -0500

    Add shader compatible seep rendering
    Finally!

--- src/client/java/moriyashiine/aylyth/client/render/block/entity/SeepBlockEntityRenderer.java
+++ src/client/java/moriyashiine/aylyth/client/render/block/entity/SeepBlockEntityRenderer.java
@@ -2,31 +2,49 @@ package moriyashiine.aylyth.client.render.block.entity;
 
 import moriyashiine.aylyth.client.render.AylythRenderLayers;
 import moriyashiine.aylyth.common.block.entity.SeepBlockEntity;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import moriyashiine.aylyth.common.util.AylythUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import net.irisshaders.iris.api.v0.IrisApi;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
 
-public class SeepBlockEntityRenderer implements BlockEntityRenderer<SeepBlockEntity> { // TODO: just have different seep blocks duh, not dynamic rendering. IIRC can use dynamic fabric models for this
+public class SeepBlockEntityRenderer implements BlockEntityRenderer<SeepBlockEntity> {
 	public SeepBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
 	}
 	
 	@Override
 	public void render(SeepBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-		Matrix4f matrix = matrices.peek().getPositionMatrix();
-		VertexConsumer vertexConsumer = vertexConsumers.getBuffer(AylythRenderLayers.SEEP);
-		this.renderSide(matrix, vertexConsumer, 0.0625F, 0.9375F, 0, 1, 0.9375F, 0.9375F, 0.9375F, 0.9375F);
-		this.renderSide(matrix, vertexConsumer, 0.0625F, 0.9375F, 1, 0, 0.0625F, 0.0625F, 0.0625F, 0.0625F);
-		this.renderSide(matrix, vertexConsumer, 0.9375F, 0.9375F, 1, 0, 0.0625F, 0.9375F, 0.9375F, 0.0625F);
-		this.renderSide(matrix, vertexConsumer, 0.0625F, 0.0625F, 0, 1, 0.0625F, 0.9375F, 0.9375F, 0.0625F);
+		MatrixStack.Entry matrix = matrices.peek();
+		VertexConsumer vertexConsumer;
+		if (!FabricLoader.getInstance().isModLoaded("iris") || !IrisApi.getInstance().isShaderPackInUse()) {
+			vertexConsumer = vertexConsumers.getBuffer(AylythRenderLayers.SEEP);
+			this.renderSide(matrix.getPositionMatrix(), vertexConsumer, 0.0625F, 0.9375F, 0, 1, 0.9375F, 0.9375F);
+			this.renderSide(matrix.getPositionMatrix(), vertexConsumer, 0.0625F, 0.9375F, 1, 0, 0.0625F, 0.0625F);
+			this.renderSide(matrix.getPositionMatrix(), vertexConsumer, 0.9375F, 0.9375F, 1, 0, 0.0625F, 0.9375F);
+			this.renderSide(matrix.getPositionMatrix(), vertexConsumer, 0.0625F, 0.0625F, 0, 1, 0.0625F, 0.9375F);
+		} else {
+			vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntitySolid(AylythUtil.id("textures/environment/seep_overlay.png")));
+			this.renderCompatSide(matrix, vertexConsumer, 0.0625F, 0.9375F, 0, 1, 0.9375F, 0.9375F);
+			this.renderCompatSide(matrix, vertexConsumer, 0.0625F, 0.9375F, 1, 0, 0.0625F, 0.0625F);
+			this.renderCompatSide(matrix, vertexConsumer, 0.9375F, 0.9375F, 1, 0, 0.0625F, 0.9375F);
+			this.renderCompatSide(matrix, vertexConsumer, 0.0625F, 0.0625F, 0, 1, 0.0625F, 0.9375F);
+		}
 	}
-	
-	private void renderSide(Matrix4f model, VertexConsumer vertices, float x1, float x2, float y1, float y2, float z1, float z2, float z3, float z4) {
+
+	private void renderCompatSide(MatrixStack.Entry entry, VertexConsumer vertices, float x1, float x2, float y1, float y2, float z1, float z2) {
+		vertices.vertex(entry.getPositionMatrix(), x1, y1, z1).color(1, 1, 1, 1f).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).normal(entry.getNormalMatrix(), 0, 0, 0).next();
+		vertices.vertex(entry.getPositionMatrix(), x2, y1, z2).color(1, 1, 1, 1f).texture(0, 1).overlay(OverlayTexture.DEFAULT_UV).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).normal(entry.getNormalMatrix(), 0, 0, 0).next();
+		vertices.vertex(entry.getPositionMatrix(), x2, y2, z2).color(1, 1, 1, 1f).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).normal(entry.getNormalMatrix(), 0, 0, 0).next();
+		vertices.vertex(entry.getPositionMatrix(), x1, y2, z1).color(1, 1, 1, 1f).texture(1, 0).overlay(OverlayTexture.DEFAULT_UV).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).normal(entry.getNormalMatrix(), 0, 0, 0).next();
+	}
+
+	private void renderSide(Matrix4f model, VertexConsumer vertices, float x1, float x2, float y1, float y2, float z1, float z2) {
 		vertices.vertex(model, x1, y1, z1).next();
 		vertices.vertex(model, x2, y1, z2).next();
-		vertices.vertex(model, x2, y2, z3).next();
-		vertices.vertex(model, x1, y2, z4).next();
+		vertices.vertex(model, x2, y2, z2).next();
+		vertices.vertex(model, x1, y2, z1).next();
 	}
 }
