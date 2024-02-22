3688b20945c38ad888654f2323a39dde0e709787
dhyces <10985914+dhyces@users.noreply.github.com>
2024-02-21 21:42:07 -0500

    Update BufferBuilderStorageMixin.java

--- src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
+++ src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BufferBuilderStorage.class)
 public class BufferBuilderStorageMixin {
 
-    // TODO: Make this toggleable for sodium/iris support
     @Inject(method = "method_22999", at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"))
     private void aylyth_assignBufferBuilder(Object2ObjectLinkedOpenHashMap<RenderLayer, BufferBuilder> map, CallbackInfo ci) {
         map.put(AylythRenderLayers.TINT, new BufferBuilder(AylythRenderLayers.TINT.getExpectedBufferSize()));
