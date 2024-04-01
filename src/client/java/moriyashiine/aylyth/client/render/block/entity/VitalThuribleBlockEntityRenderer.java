b29a8c844aac147d0147680d90aa0c6201de4bec
dhyces <10985914+dhyces@users.noreply.github.com>
2024-03-31 20:32:41 -0400

    Further revise BER for Vital Thurible

--- src/client/java/moriyashiine/aylyth/client/render/block/entity/VitalThuribleBlockEntityRenderer.java
+++ src/client/java/moriyashiine/aylyth/client/render/block/entity/VitalThuribleBlockEntityRenderer.java
@@ -1,6 +1,5 @@
 package moriyashiine.aylyth.client.render.block.entity;
 
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import moriyashiine.aylyth.common.block.entity.VitalThuribleBlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -9,22 +8,19 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import org.joml.Vector4f;
 import org.joml.Vector4fc;
 
-import java.util.List;
-
 public class VitalThuribleBlockEntityRenderer implements BlockEntityRenderer<VitalThuribleBlockEntity> {
-    private static final List<Vector4fc> POS = Util.make(new ObjectArrayList<>(), list -> {
-        list.add(new Vector4f(-0.1F, 0.03F, -0.1F, 25 * MathHelper.RADIANS_PER_DEGREE));
-        list.add(new Vector4f(0.1F, 0.06F, -0.1F, -45 * MathHelper.RADIANS_PER_DEGREE));
-        list.add(new Vector4f(0.1F, 0.01F, 0.1F, 15 * MathHelper.RADIANS_PER_DEGREE));
-        list.add(new Vector4f(-0.1F, 0.07F, 0.1F, 65 * MathHelper.RADIANS_PER_DEGREE));
-        list.add(new Vector4f(0.05F, 0.1F, 0.05F, -35 * MathHelper.RADIANS_PER_DEGREE));
-    });
+    private static final Vector4fc[] POS = {
+            new Vector4f(-0.1F, 0.03F, -0.1F, 25 * MathHelper.RADIANS_PER_DEGREE),
+            new Vector4f(0.1F, 0.06F, -0.1F, -45 * MathHelper.RADIANS_PER_DEGREE),
+            new Vector4f(0.1F, 0.01F, 0.1F, 15 * MathHelper.RADIANS_PER_DEGREE),
+            new Vector4f(-0.1F, 0.07F, 0.1F, 65 * MathHelper.RADIANS_PER_DEGREE),
+            new Vector4f(0.05F, 0.1F, 0.05F, -35 * MathHelper.RADIANS_PER_DEGREE)
+    };
 
     public VitalThuribleBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
     }
@@ -32,8 +28,8 @@ public class VitalThuribleBlockEntityRenderer implements BlockEntityRenderer<Vit
     @Override
     public void render(VitalThuribleBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.translate(0.5, 0.25, 0.5);
-        for(int i = 0; i < entity.getStack(0).getCount(); i++){
-            Vector4fc transform = POS.get(i);
+        for (int i = 0; i < entity.getStack(0).getCount(); i++) {
+            Vector4fc transform = POS[i];
             ItemStack stack = entity.getStack(0);
             matrices.push();
             matrices.translate(transform.x(), transform.y(), transform.z());
