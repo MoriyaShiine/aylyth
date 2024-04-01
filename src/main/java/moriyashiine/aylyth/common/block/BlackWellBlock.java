aff4713264765f0611ae4c2bf1076cf137aabf2e
dhyces <10985914+dhyces@users.noreply.github.com>
2024-03-31 22:33:58 -0400

    Update black well block w/ load safety

--- src/main/java/moriyashiine/aylyth/common/block/BlackWellBlock.java
+++ src/main/java/moriyashiine/aylyth/common/block/BlackWellBlock.java
@@ -1,9 +1,9 @@
 package moriyashiine.aylyth.common.block;
 
+import com.google.common.base.Suppliers;
 import moriyashiine.aylyth.common.registry.ModPotions;
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -19,8 +19,11 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.function.Supplier;
+
+@SuppressWarnings("UnstableApiUsage")
 public class BlackWellBlock extends Block {
-    private static final ItemVariant BLIGHT_POTION = ItemVariant.of(PotionUtil.setPotion(new ItemStack(Items.POTION), ModPotions.BLIGHT_POTION));
+    private static final Supplier<ItemVariant> BLIGHT_POTION = Suppliers.memoize(() -> ItemVariant.of(PotionUtil.setPotion(new ItemStack(Items.POTION), ModPotions.BLIGHT_POTION)));
 
     public BlackWellBlock(Settings settings) {
         super(settings);
@@ -31,7 +34,7 @@ public class BlackWellBlock extends Block {
         ContainerItemContext storage = ContainerItemContext.forPlayerInteraction(player, hand);
         try (Transaction transaction = Transaction.openOuter()) {
             if (storage.getItemVariant().isOf(Items.GLASS_BOTTLE)) {
-                if (storage.exchange(BLIGHT_POTION, 1, transaction) == 1) {
+                if (storage.exchange(BLIGHT_POTION.get(), 1, transaction) == 1) {
                     transaction.commit();
                 }
                 world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.PLAYERS, 1.0f, 1.0f);
