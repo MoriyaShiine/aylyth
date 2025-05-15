660abadf53d933071e449498a797426878fa3e8b
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-15 16:33:11 -0400

    Update loot context types

--- src/main/java/moriyashiine/aylyth/common/loot/AylythLootContextTypes.java
+++ src/main/java/moriyashiine/aylyth/common/loot/AylythLootContextTypes.java
@@ -3,24 +3,24 @@ package moriyashiine.aylyth.common.loot;
 import moriyashiine.aylyth.common.Aylyth;
 import moriyashiine.aylyth.mixin.LootContextTypesAccessor;
 import net.minecraft.loot.context.LootContextParameters;
-import net.minecraft.loot.context.LootContextType;
+import net.minecraft.util.context.ContextType;
 
 import java.util.function.UnaryOperator;
 
 public interface AylythLootContextTypes {
-    LootContextType HARVEST = register("harvest", builder ->
+    ContextType HARVEST = register("harvest", builder ->
             builder.require(LootContextParameters.ORIGIN)
                     .require(LootContextParameters.BLOCK_STATE)
                     .allow(LootContextParameters.TOOL)
     );
-    LootContextType STRIP = register("strip", builder ->
+    ContextType STRIP = register("strip", builder ->
             builder.require(LootContextParameters.ORIGIN)
                     .require(LootContextParameters.BLOCK_STATE)
                     .allow(LootContextParameters.TOOL)
     );
 
-    private static LootContextType register(String name, UnaryOperator<LootContextType.Builder> builder) {
-        LootContextType type = builder.apply(LootContextType.create()).build();
+    private static ContextType register(String name, UnaryOperator<ContextType.Builder> builder) {
+        ContextType type = builder.apply(new ContextType.Builder()).build();
         LootContextTypesAccessor.getMap().put(Aylyth.id(name), type);
         return type;
     }
--- src/main/java/moriyashiine/aylyth/mixin/LootContextTypesAccessor.java
+++ src/main/java/moriyashiine/aylyth/mixin/LootContextTypesAccessor.java
@@ -1,16 +1,16 @@
 package moriyashiine.aylyth.mixin;
 
 import com.google.common.collect.BiMap;
-import net.minecraft.loot.context.LootContextType;
 import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.context.ContextType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(LootContextTypes.class)
 public interface LootContextTypesAccessor {
-    @Accessor
-    static BiMap<Identifier, LootContextType> getMap() {
+    @Accessor("MAP")
+    static BiMap<Identifier, ContextType> getMap() {
         throw new AssertionError("Implemented via mixin");
     }
 }
