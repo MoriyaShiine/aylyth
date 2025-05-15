6476dd3ecb10ebfa2fe715e88e58bc080776c646
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-15 16:33:19 -0400

    Update potions

--- src/main/java/moriyashiine/aylyth/common/item/potion/AylythPotions.java
+++ src/main/java/moriyashiine/aylyth/common/item/potion/AylythPotions.java
@@ -7,23 +7,24 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.potion.Potion;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public interface AylythPotions {
 
-	Potion MORTECHIS = registerBase(
+	RegistryEntry<Potion> MORTECHIS = registerBase(
 			"mortechis",
 			new StatusEffectInstance(AylythStatusEffects.MORTECHIS, 3600),
 			new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0),
 			new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0)
 	);
-	Potion LONG_MORTECHIS = registerUpgraded(
+	RegistryEntry<Potion> LONG_MORTECHIS = registerUpgraded(
 			"long_mortechis",
 			"mortechis",
 			new StatusEffectInstance(AylythStatusEffects.MORTECHIS, 9600),
 			new StatusEffectInstance(StatusEffects.NAUSEA, 400, 0),
 			new StatusEffectInstance(StatusEffects.REGENERATION, 400, 0)
 	);
-	Potion STRONG_MORTECHIS = registerUpgraded(
+	RegistryEntry<Potion> STRONG_MORTECHIS = registerUpgraded(
 			"strong_mortechis",
 			"mortechis",
 			new StatusEffectInstance(AylythStatusEffects.MORTECHIS, 1800, 1),
@@ -31,27 +32,27 @@ public interface AylythPotions {
 			new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0)
 	);
 
-	Potion CIMMERIAN = registerBase("cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 3600));
-	Potion LONG_CIMMERIAN = registerUpgraded("long_cimmerian", "cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 9600));
-	Potion STRONG_CIMMERIAN = registerUpgraded("strong_cimmerian", "cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 1800, 1));
+	RegistryEntry<Potion> CIMMERIAN = registerBase("cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 3600));
+	RegistryEntry<Potion> LONG_CIMMERIAN = registerUpgraded("long_cimmerian", "cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 9600));
+	RegistryEntry<Potion> STRONG_CIMMERIAN = registerUpgraded("strong_cimmerian", "cimmerian", new StatusEffectInstance(AylythStatusEffects.CIMMERIAN, 1800, 1));
 
-	Potion WYRDED = registerBase("wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 3600));
-	Potion LONG_WYRDED = registerUpgraded("long_wyrded", "wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 9600));
-	Potion STRONG_WYRDED = registerUpgraded("strong_wyrded", "wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 1800, 1));
+	RegistryEntry<Potion> WYRDED = registerBase("wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 3600));
+	RegistryEntry<Potion> LONG_WYRDED = registerUpgraded("long_wyrded", "wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 9600));
+	RegistryEntry<Potion> STRONG_WYRDED = registerUpgraded("strong_wyrded", "wyrded", new StatusEffectInstance(AylythStatusEffects.WYRDED, 1800, 1));
 
-	Potion BLIGHT = registerBase("blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 3600));
-	Potion LONG_BLIGHT = registerUpgraded("long_blight", "blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 9600));
-	Potion STRONG_BLIGHT = registerUpgraded("strong_blight", "blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 1800, 1));
+	RegistryEntry<Potion> BLIGHT = registerBase("blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 3600));
+	RegistryEntry<Potion> LONG_BLIGHT = registerUpgraded("long_blight", "blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 9600));
+	RegistryEntry<Potion> STRONG_BLIGHT = registerUpgraded("strong_blight", "blight", new StatusEffectInstance(AylythStatusEffects.BLIGHT, 1800, 1));
 
-	private static Potion register(String name, Potion potion) {
-		return Registry.register(Registries.POTION, Aylyth.id(name), potion);
+	private static RegistryEntry<Potion> register(String name, Potion potion) {
+		return Registry.registerReference(Registries.POTION, Aylyth.id(name), potion);
 	}
 
-	private static Potion registerBase(String name, StatusEffectInstance... effects) {
-		return register(name, new Potion(effects));
+	private static RegistryEntry<Potion> registerBase(String name, StatusEffectInstance... effects) {
+		return register(name, new Potion(name, effects));
 	}
 
-	private static Potion registerUpgraded(String registryName, String potionName, StatusEffectInstance... effects) {
+	private static RegistryEntry<Potion> registerUpgraded(String registryName, String potionName, StatusEffectInstance... effects) {
 		return register(registryName, new Potion(potionName, effects));
 	}
 
