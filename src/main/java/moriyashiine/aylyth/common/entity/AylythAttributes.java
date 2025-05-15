0859d3127987d47bb80e77d3c74d6275ab4f926a
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-14 20:37:03 -0400

    Update attributes

--- src/main/java/moriyashiine/aylyth/common/entity/AylythAttributes.java
+++ src/main/java/moriyashiine/aylyth/common/entity/AylythAttributes.java
@@ -5,13 +5,14 @@ import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public interface AylythAttributes {
 
-    EntityAttribute MAX_VITAL_HEALTH = register("player.max_vital_health", new ClampedEntityAttribute("attribute.name.player.max_vital_health", 0, 0, 1024.0).setTracked(true));
+    RegistryEntry<EntityAttribute> MAX_VITAL_HEALTH = register("max_vital_health", new ClampedEntityAttribute("attribute.name.max_vital_health", 0, 0, 1024.0).setTracked(true));
 
-    private static <A extends EntityAttribute> A register(String name, A attribute) {
-        return Registry.register(Registries.ATTRIBUTE, Aylyth.id(name), attribute);
+    private static <A extends EntityAttribute> RegistryEntry<EntityAttribute> register(String name, A attribute) {
+        return Registry.registerReference(Registries.ATTRIBUTE, Aylyth.id(name), attribute);
     }
 
     // Load static initializer
