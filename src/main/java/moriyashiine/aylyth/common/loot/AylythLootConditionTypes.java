0b7aa7b84f4ce34012aa9a03adeb42fe625fd052
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-16 16:01:27 -0400

    Update AylythLootConditionTypes.java

--- src/main/java/moriyashiine/aylyth/common/loot/AylythLootConditionTypes.java
+++ src/main/java/moriyashiine/aylyth/common/loot/AylythLootConditionTypes.java
@@ -1,16 +1,16 @@
 package moriyashiine.aylyth.common.loot;
 
+import com.mojang.serialization.MapCodec;
 import moriyashiine.aylyth.common.Aylyth;
 import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.condition.LootConditionType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.JsonSerializer;
 
 public interface AylythLootConditionTypes {
 
-    private static <C extends LootCondition> LootConditionType register(String name, JsonSerializer<C> serializer) {
-        return Registry.register(Registries.LOOT_CONDITION_TYPE, Aylyth.id(name), new LootConditionType(serializer));
+    private static <C extends LootCondition> LootConditionType register(String name, MapCodec<C> codec) {
+        return Registry.register(Registries.LOOT_CONDITION_TYPE, Aylyth.id(name), new LootConditionType(codec));
     }
 
     // Load static initializer
