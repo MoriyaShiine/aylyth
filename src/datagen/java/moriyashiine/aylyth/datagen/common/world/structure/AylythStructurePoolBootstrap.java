6616ceccd3b2bd1d96570927a9477f4618faa854
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-18 01:01:28 -0400

    Update structure bootstrap

--- src/datagen/java/moriyashiine/aylyth/datagen/common/world/structure/AylythStructurePoolBootstrap.java
+++ src/datagen/java/moriyashiine/aylyth/datagen/common/world/structure/AylythStructurePoolBootstrap.java
@@ -10,12 +10,14 @@ import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.structure.StructureLiquidSettings;
 import net.minecraft.structure.pool.SinglePoolElement;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.processor.StructureProcessorList;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
+import java.util.Optional;
 
 public final class AylythStructurePoolBootstrap {
     private AylythStructurePoolBootstrap () {}
@@ -25,14 +27,25 @@ public final class AylythStructurePoolBootstrap {
         RegistryEntry<StructurePool> emptyPool = poolRegistry.getOrThrow(RegistryKey.of(RegistryKeys.TEMPLATE_POOL, Identifier.of("empty")));
         RegistryEntryLookup<StructureProcessorList> processorListRegistry = context.getRegistryLookup(RegistryKeys.PROCESSOR_LIST);
         RegistryEntry<StructureProcessorList> blackWell = processorListRegistry.getOrThrow(RegistryKey.of(RegistryKeys.PROCESSOR_LIST, Aylyth.id("black_well")));
-        context.register(AylythStructurePools.BLACK_WELL, single(emptyPool, Aylyth.id("black_well"), blackWell, StructurePool.Projection.TERRAIN_MATCHING));
+        context.register(AylythStructurePools.BLACK_WELL, single(emptyPool, Aylyth.id("black_well"), blackWell, StructurePool.Projection.TERRAIN_MATCHING, Optional.empty()));
     }
 
-    private static StructurePool single(RegistryEntry<StructurePool> fallback, Identifier id, RegistryEntry<StructureProcessorList> processorList, StructurePool.Projection projection) {
-        return new StructurePool(fallback, List.of(Pair.of(fromId(id, processorList, projection), 1)));
+    private static StructurePool single(
+            RegistryEntry<StructurePool> fallback,
+            Identifier id,
+            RegistryEntry<StructureProcessorList> processorList,
+            StructurePool.Projection projection,
+            Optional<StructureLiquidSettings> overrideLiquidSettings
+    ) {
+        return new StructurePool(fallback, List.of(Pair.of(fromId(id, processorList, projection, overrideLiquidSettings), 1)));
     }
 
-    private static SinglePoolElement fromId(Identifier id, RegistryEntry<StructureProcessorList> processorList, StructurePool.Projection projection) {
-        return SinglePoolElementInvoker.invokeInit(Either.left(id), processorList, projection);
+    private static SinglePoolElement fromId(
+            Identifier id,
+            RegistryEntry<StructureProcessorList> processorList,
+            StructurePool.Projection projection,
+            Optional<StructureLiquidSettings> overrideLiquidSettings
+    ) {
+        return SinglePoolElementInvoker.invokeInit(Either.left(id), processorList, projection, overrideLiquidSettings);
     }
 }
--- src/datagen/java/moriyashiine/aylyth/datagen/mixin/SinglePoolElementInvoker.java
+++ src/datagen/java/moriyashiine/aylyth/datagen/mixin/SinglePoolElementInvoker.java
@@ -2,6 +2,7 @@ package moriyashiine.aylyth.datagen.mixin;
 
 import com.mojang.datafixers.util.Either;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.structure.StructureLiquidSettings;
 import net.minecraft.structure.StructureTemplate;
 import net.minecraft.structure.pool.SinglePoolElement;
 import net.minecraft.structure.pool.StructurePool;
@@ -10,10 +11,17 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
+import java.util.Optional;
+
 @Mixin(SinglePoolElement.class)
 public interface SinglePoolElementInvoker {
     @Invoker("<init>")
-    static SinglePoolElement invokeInit(Either<Identifier, StructureTemplate> location, RegistryEntry<StructureProcessorList> processors, StructurePool.Projection projection) {
+    static SinglePoolElement invokeInit(
+            Either<Identifier, StructureTemplate> location,
+            RegistryEntry<StructureProcessorList> processors,
+            StructurePool.Projection projection,
+            Optional<StructureLiquidSettings> overrideLiquidSettings
+    ) {
         throw new AssertionError("Implemented via mixin");
     }
 }
