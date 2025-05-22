97afcbc722e2d18a6b5cfedffb3f5d6f51754920
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:52:00 -0400

    Comment out loot modifier for now

--- src/main/java/moriyashiine/aylyth/common/loot/AylythModifyLootTableHandler.java
+++ src/main/java/moriyashiine/aylyth/common/loot/AylythModifyLootTableHandler.java
@@ -1,22 +1,26 @@
 package moriyashiine.aylyth.common.loot;
 
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableSource;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryWrapper;
 
 public final class AylythModifyLootTableHandler {
     private AylythModifyLootTableHandler() {}
 
     public static void register() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (!id.getPath().startsWith("additions/")) {
-                LootTable table = lootManager.getLootTable(id.withPrefixedPath("additions/"));
-                if (table != LootTable.EMPTY) {
-                    for (LootPool pool : table.pools) {
-                        tableBuilder.pool(pool);
-                    }
-                }
-            }
-        });
+        // TODO: Figure out how to mend this
+//        LootTableEvents.MODIFY.register((RegistryKey<LootTable> key, LootTable.Builder tableBuilder, LootTableSource source, RegistryWrapper.WrapperLookup registries) -> {
+//            if (!key.getValue().getPath().startsWith("additions/")) {
+//                LootTable table = lootManager.getLootTable(id.withPrefixedPath("additions/"));
+//                if (table != LootTable.EMPTY) {
+//                    for (LootPool pool : table.pools) {
+//                        tableBuilder.pool(pool);
+//                    }
+//                }
+//            }
+//        });
     }
 }
