64e96f4f208507260e5026ec847d531406a6494c
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:02:27 -0400

    Update ZoglinEntityMixin.java

--- src/main/java/moriyashiine/aylyth/mixin/cimmerian/ZoglinEntityMixin.java
+++ src/main/java/moriyashiine/aylyth/mixin/cimmerian/ZoglinEntityMixin.java
@@ -3,6 +3,7 @@ package moriyashiine.aylyth.mixin.cimmerian;
 import moriyashiine.aylyth.common.entity.AylythStatusEffects;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.ZoglinEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ZoglinEntityMixin {
 
     @Inject(method = "shouldAttack", at = @At("HEAD"), cancellable = true)
-    private void aylyth_shouldAttack(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (entity.hasStatusEffect(AylythStatusEffects.CIMMERIAN)) {
+    private void aylyth_shouldAttack(ServerWorld world, LivingEntity target, CallbackInfoReturnable<Boolean> cir) {
+        if (target.hasStatusEffect(AylythStatusEffects.CIMMERIAN)) {
             cir.setReturnValue(false);
         }
     }
