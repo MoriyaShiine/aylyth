e558d8c4afa4410992a51247575fbea2943bfeed
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:51:42 -0400

    Update EnchantmentTargetMixin.java

--- src/main/java/moriyashiine/aylyth/mixin/EnchantmentTargetMixin.java
+++ src/main/java/moriyashiine/aylyth/mixin/EnchantmentTargetMixin.java
@@ -1,18 +1,19 @@
-package moriyashiine.aylyth.mixin;
-
-import moriyashiine.aylyth.common.item.types.YmpeLanceItem;
-import net.minecraft.item.Item;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-@Mixin(targets = "net/minecraft/enchantment/EnchantmentTarget$9")
-public class EnchantmentTargetMixin {
-    @Inject(method = "isAcceptableItem(Lnet/minecraft/item/Item;)Z", at = @At("HEAD"), cancellable = true)
-    private void aylyth_lanceIsAcceptableItem(Item item, CallbackInfoReturnable<Boolean> cir) {
-        if (item instanceof YmpeLanceItem){
-            cir.setReturnValue(true);
-        }
-    }
-}
+// TODO: Add lance to trident enchantments
+//package moriyashiine.aylyth.mixin;
+//
+//import moriyashiine.aylyth.common.item.types.YmpeLanceItem;
+//import net.minecraft.item.Item;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+//
+//@Mixin(targets = "net/minecraft/enchantment/EnchantmentTarget$9")
+//public class EnchantmentTargetMixin {
+//    @Inject(method = "isAcceptableItem(Lnet/minecraft/item/Item;)Z", at = @At("HEAD"), cancellable = true)
+//    private void aylyth_lanceIsAcceptableItem(Item item, CallbackInfoReturnable<Boolean> cir) {
+//        if (item instanceof YmpeLanceItem){
+//            cir.setReturnValue(true);
+//        }
+//    }
+//}
