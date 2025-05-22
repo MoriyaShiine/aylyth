30df9df94303a857fd19b38030da2f6eb156c6f6
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:00:35 -0400

    Update BufferBuilderStorageMixin.java

--- src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
+++ src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
@@ -1,20 +1,25 @@
 package moriyashiine.aylyth.mixin.client;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
+import com.llamalad7.mixinextras.sugar.Local;
 import moriyashiine.aylyth.client.render.AylythRenderLayers;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.BufferAllocator;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.SequencedMap;
+
+// TODO: Remove debug
+@Debug(export = true)
 @Mixin(BufferBuilderStorage.class)
 public class BufferBuilderStorageMixin {
 
-    @Inject(method = "method_22999", at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"))
-    private void aylyth_assignBufferBuilder(Object2ObjectLinkedOpenHashMap<RenderLayer, BufferBuilder> map, CallbackInfo ci) {
-        map.put(AylythRenderLayers.TINT, new BufferBuilder(AylythRenderLayers.TINT.getExpectedBufferSize()));
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;immediate(Ljava/util/SequencedMap;Lnet/minecraft/client/util/BufferAllocator;)Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;"))
+    private void aylyth_assignBufferBuilder(int maxBlockBuildersPoolSize, CallbackInfo ci, @Local(ordinal = 0) SequencedMap<RenderLayer, BufferAllocator> sequencedMap) {
+        sequencedMap.put(AylythRenderLayers.TINT, new BufferAllocator(AylythRenderLayers.TINT.getExpectedBufferSize()));
     }
 }
