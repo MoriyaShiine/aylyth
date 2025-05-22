e71ff75478ad45db7753c4f561190fd1bbce65c6
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:00:48 -0400

    Update MinecraftClientMixin.java

--- src/client/java/moriyashiine/aylyth/mixin/client/MinecraftClientMixin.java
+++ src/client/java/moriyashiine/aylyth/mixin/client/MinecraftClientMixin.java
@@ -1,18 +1,12 @@
 package moriyashiine.aylyth.mixin.client;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.sugar.Local;
-import moriyashiine.aylyth.common.data.world.AylythDimensionData;
 import moriyashiine.aylyth.common.item.AylythItems;
 import moriyashiine.aylyth.common.network.packets.GlaivePacketC2S;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.sound.MusicSound;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.world.biome.Biome;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -32,30 +26,32 @@ public abstract class MinecraftClientMixin {
     @Unique
     private boolean attackQueued = false;
 
-    @Inject(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;doAttack()Z", ordinal = 0))
+    @Inject(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;doAttack()Z"))
     public void aylyth_glaiveStab(CallbackInfo info) {
-        if(player != null) {
-            if(player.getStackInHand(player.getActiveHand()).isOf(AylythItems.YMPE_GLAIVE)) {
-                if(player.getAttackCooldownProgress(0.5F) == 1F && (!player.getItemCooldownManager().isCoolingDown(player.getMainHandStack().getItem())) && crosshairTarget != null) {
+        if (player != null) {
+            if (player.getStackInHand(player.getActiveHand()).isOf(AylythItems.YMPE_GLAIVE)) {
+                if (player.getAttackCooldownProgress(0.5F) == 1F && (!player.getItemCooldownManager().isCoolingDown(player.getMainHandStack())) && crosshairTarget != null) {
                     if (crosshairTarget instanceof EntityHitResult entityHitResult) {
                         ClientPlayNetworking.send(new GlaivePacketC2S(entityHitResult.getEntity().getId()));
                     }
 
-                    if(crosshairTarget.getType() == HitResult.Type.BLOCK)
+                    if (crosshairTarget.getType() == HitResult.Type.BLOCK) {
                         player.resetLastAttackedTicks();
+                    }
                 }
             }
         }
 
-        if(!info.isCancelled() && attackQueued)
+        if(!info.isCancelled() && attackQueued) {
             attackQueued = false;
+        }
     }
 
-    @ModifyReturnValue(method = "getMusicType", at = @At(value = "RETURN", ordinal = 4))
-    private MusicSound aylyth$getMusicType(MusicSound original, @Local RegistryEntry<Biome> biome) {
-        if (player.getWorld().getRegistryKey() == AylythDimensionData.WORLD) {
-            return biome.value().getMusic().orElse(original);
-        }
-        return original;
-    }
+//    @ModifyReturnValue(method = "getMusicInstance", at = @At(value = "RETURN", ordinal = 4))
+//    private MusicInstance aylyth$getMusicType(MusicInstance original, @Local RegistryEntry<Biome> biome, @Local World world) {
+//        if (player.getWorld().getRegistryKey() == AylythDimensionData.WORLD) {
+//            return biome.value().getMusic().map(pool -> pool.getDataOrEmpty(world.random)).orElse(Optional.of(original));
+//        }
+//        return original;
+//    }
 }
