4a08bba6e1f0c561f6334abe17b33028b77ed33e
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 16:02:33 -0400

    Create LivingEntityAccessor.java

---
+++ src/main/java/moriyashiine/aylyth/mixin/LivingEntityAccessor.java
@@ -0,0 +1,12 @@
+package moriyashiine.aylyth.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Invoker
+    Vec3d invokeApplyMovementInput(Vec3d movementInput, float slipperiness);
+}
