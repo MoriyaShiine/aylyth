169206b1faf58450d57296366f337dd6c18c6034
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-22 19:44:21 -0400

    Fix carver bootstrap

--- src/datagen/java/moriyashiine/aylyth/datagen/common/world/feature/AylythConfiguredCarverBootstrap.java
+++ src/datagen/java/moriyashiine/aylyth/datagen/common/world/feature/AylythConfiguredCarverBootstrap.java
@@ -1,8 +1,11 @@
 package moriyashiine.aylyth.datagen.common.world.feature;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryEntryLookup;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.floatprovider.ConstantFloatProvider;
 import net.minecraft.util.math.floatprovider.TrapezoidFloatProvider;
@@ -17,6 +20,7 @@ public final class AylythConfiguredCarverBootstrap {
     private AylythConfiguredCarverBootstrap() {}
 
     public static void bootstrap(Registerable<ConfiguredCarver<?>> context) {
+        RegistryEntryLookup<Block> blockRegistry = context.getRegistryLookup(RegistryKeys.BLOCK);
         context.register(CAVES, Carver.CAVE.configure(
                 new CaveCarverConfig(
                         0.35F,
@@ -24,7 +28,7 @@ public final class AylythConfiguredCarverBootstrap {
                         UniformFloatProvider.create(0.1F, 1.9F),
                         YOffset.getBottom(),
                         CarverDebugConfig.create(false, Blocks.CRIMSON_BUTTON.getDefaultState()),
-                        Registries.BLOCK.getOrThrow(BlockTags.OVERWORLD_CARVER_REPLACEABLES),
+                        blockRegistry.getOrThrow(BlockTags.OVERWORLD_CARVER_REPLACEABLES),
                         UniformFloatProvider.create(0.7F, 1.4F),
                         UniformFloatProvider.create(0.8F, 1.3F),
                         UniformFloatProvider.create(-1.0F, -0.4F)
@@ -37,7 +41,7 @@ public final class AylythConfiguredCarverBootstrap {
                         ConstantFloatProvider.create(3.0F),
                         YOffset.getBottom(),
                         CarverDebugConfig.create(false, Blocks.WARPED_BUTTON.getDefaultState()),
-                        Registries.BLOCK.getOrThrow(BlockTags.OVERWORLD_CARVER_REPLACEABLES),
+                        blockRegistry.getOrThrow(BlockTags.OVERWORLD_CARVER_REPLACEABLES),
                         UniformFloatProvider.create(-0.125F, 0.125F),
                         new RavineCarverConfig.Shape(
                                 UniformFloatProvider.create(0.75F, 1.0F), TrapezoidFloatProvider.create(0.0F, 6.0F, 2.0F), 3, UniformFloatProvider.create(0.75F, 1.0F), 1.0F, 0.0F
