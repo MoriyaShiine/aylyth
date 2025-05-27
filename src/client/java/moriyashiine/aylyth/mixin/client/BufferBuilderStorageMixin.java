94ae59018504986882f12522c84e14b780adfc43
dhyces <10985914+dhyces@users.noreply.github.com>
2025-05-26 20:53:55 -0400

    Only add injection once

--- src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
+++ src/client/java/moriyashiine/aylyth/mixin/client/BufferBuilderStorageMixin.java
@@ -5,7 +5,6 @@ import moriyashiine.aylyth.client.render.AylythRenderLayers;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.BufferAllocator;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,12 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.SequencedMap;
 
-// TODO: Remove debug
-@Debug(export = true)
 @Mixin(BufferBuilderStorage.class)
 public class BufferBuilderStorageMixin {
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;immediate(Ljava/util/SequencedMap;Lnet/minecraft/client/util/BufferAllocator;)Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;"))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;immediate(Ljava/util/SequencedMap;Lnet/minecraft/client/util/BufferAllocator;)Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;", ordinal = 0))
     private void aylyth_assignBufferBuilder(int maxBlockBuildersPoolSize, CallbackInfo ci, @Local(ordinal = 0) SequencedMap<RenderLayer, BufferAllocator> sequencedMap) {
         sequencedMap.put(AylythRenderLayers.TINT, new BufferAllocator(AylythRenderLayers.TINT.getExpectedBufferSize()));
     }
